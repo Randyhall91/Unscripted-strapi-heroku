b8f70d30f3199d8c12f72574f032f3dfc79b0ef0
Thesommoner <randyrocks11@gmail.com>
2022-12-27 19:51:46 -0700

    investmentpage

---
+++ src/api/investmentpage/controllers/investmentpage.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * investmentpage controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::investmentpage.investmentpage');

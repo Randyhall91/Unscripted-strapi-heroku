84bc386770531b570843df6cc0d6530a7c45524c
Thesommoner <randyrocks11@gmail.com>
2023-01-04 20:25:05 -0700

    portfolio

--- src/api/profile-page/controllers/profile-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * profile-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::profile-page.profile-page');
--- src/api/profile-page/routes/profile-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * profile-page router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::profile-page.profile-page');
